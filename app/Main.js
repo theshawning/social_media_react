308e1f846a54c4f273b183f450ae68100bb27d00
Shawn Harrison <38034484+theshawning@users.noreply.github.com>
2020-05-01 13:58:51 +0000

    created Terms and About pages and connected them all to Main.js through React Router

--- app/Main.js
+++ app/Main.js
@@ -1,18 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {BrowserRouter, Switch, Route} from 'react-router-dom';
 
 import Header from './components/Header';
 import HomeGuest from './components/HomeGuest';
 import Footer from './components/Footer';
+import About from './components/About';
+import Terms from './components/Terms';
 
 function App() {
   return (
-    <>
+    <BrowserRouter>
       <Header />
-      <HomeGuest />
-      <Footer />
-      
-    </>
+      <Switch>
+        <Route path='/' exact><HomeGuest /></Route>
+        <Route path='/about-us' exact><About /></Route>
+        <Route path='/terms' exact><Terms /></Route>
+      </Switch>
+      <Footer />      
+    </BrowserRouter>
   )
 }
 
