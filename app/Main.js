bbe1c93d9e3dba3318959f9e5ecafd46e2b75f23
Shawn Harrison <38034484+theshawning@users.noreply.github.com>
2020-05-02 13:00:10 +0000

    lifted loggedIn state up to Main component and created new Home component for loggedIn users

--- app/Main.js
+++ app/Main.js
@@ -1,19 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
 import {BrowserRouter, Switch, Route} from 'react-router-dom';
 
 import Header from './components/Header';
 import HomeGuest from './components/HomeGuest';
+import Home from './components/Home'
 import Footer from './components/Footer';
 import About from './components/About';
 import Terms from './components/Terms';
 
 function App() {
+  const [loggedIn, setLoggedIn] = useState(Boolean(localStorage.getItem('complexAppToken')));
+
   return (
     <BrowserRouter>
-      <Header />
+      <Header loggedIn={loggedIn} setLoggedIn={setLoggedIn} />
       <Switch>
-        <Route path='/' exact><HomeGuest /></Route>
+        <Route path='/' exact>
+          {loggedIn ? <Home /> : <HomeGuest /> }
+        </Route>
         <Route path='/about-us' exact><About /></Route>
         <Route path='/terms' exact><Terms /></Route>
       </Switch>
