502917524d3e56702893c0259e115f518ebee6fd
Shawn Harrison <38034484+theshawning@users.noreply.github.com>
2020-05-02 12:39:25 +0000

    created HeaderLoggedIn component and updated other Header components

--- app/components/HeaderLoggedOut.js
+++ app/components/HeaderLoggedOut.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import Axios from 'axios';
 
- function HeaderLoggedOut() {
+ function HeaderLoggedOut(props) {
   const [username, setUsername] = useState();
   const [password, setPassword] = useState();
 
@@ -14,6 +14,7 @@ import Axios from 'axios';
       })
       if (response.data) {
         console.log(response.data)
+        props.setLoggedIn(true)
       } else {
         console.log('Incorrect username / password.')        
       }
